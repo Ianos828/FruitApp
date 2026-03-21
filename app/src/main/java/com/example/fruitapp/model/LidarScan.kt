25860aa82b4c80095f36addfd70e79349b4b8530
kenrick4216 <e1122820@u.nus.edu>
2026-03-22 02:07:21 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/fruitapp/model/LidarScan.kt
@@ -0,0 +1,18 @@
+package com.example.fruitapp.model
+
+/**
+ * Represents the full 2D lidar scan result from the ESP32
+ */
+data class LidarScan(
+    val scan: List<LidarPoint>
+)
+
+/**
+ * A single point in the lidar scan
+ * @param step the horizontal position of the sensor (motor step index)
+ * @param mm the distance in millimetres to the fruit surface at that position
+ */
+data class LidarPoint(
+    val step: Int,
+    val mm: Int
+)
